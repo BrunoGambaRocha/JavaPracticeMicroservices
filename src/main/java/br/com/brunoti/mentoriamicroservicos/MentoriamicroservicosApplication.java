9ec793b301fc41b9842992230dc0161f6120fb00
BrunoGambaRocha <bruno.roch@gmail.com>
2021-09-14 16:21:40 -0300

    base project

---
+++ src/main/java/br/com/brunoti/mentoriamicroservicos/MentoriamicroservicosApplication.java
@@ -0,0 +1,14 @@
+package br.com.brunoti.mentoriamicroservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoriamicroservicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoriamicroservicosApplication.class, args);
+		System.out.println("Hello Microservices!!!");
+	}
+
+}
---
+++ src/test/java/br/com/brunoti/mentoriamicroservicos/MentoriamicroservicosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunoti.mentoriamicroservicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoriamicroservicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
